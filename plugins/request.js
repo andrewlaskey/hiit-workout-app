2ddb513d602fb3b525b6bfb30504344837bcc186
Andrew Laskey <andrew.laskey@gmail.com>
2020-01-27 21:51:23 -0800

    add DatoCMS

--- plugins/request.js
+++ plugins/request.js
@@ -1,8 +1,45 @@
-import exercises from './exercises'
-
 export default ({ app }, inject) => {
+  const request = (query) => {
+    return fetch(
+      process.env.DATOCMS_GRAPHQL,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${process.env.DATOCMS_TOKEN}`
+        },
+        body: JSON.stringify({
+          query
+        })
+      })
+      .then(res => res.json())
+  }
+
   const getExercises = () => {
-    return Promise.resolve(exercises)
+    return request(`
+      query getAllExercises {
+        allExercises {
+          handle
+          id
+          name
+          tags
+        }
+      }
+    `).then(res => {
+      if (res.data && res.data.allExercises) {
+        return res.data.allExercises.map(exercise => {
+          if (exercise.tags) {
+            exercise.tags = exercise.tags.split(', ')
+          }
+
+          return exercise
+        })
+      }
+    }).catch(err => {
+      console.error(err)
+      return []
+    })
   }
 
   inject('request', {
