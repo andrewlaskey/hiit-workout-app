53d6975764b91fa68d508cae1e11aa01af577527
Andrew Laskey <andrew.laskey@gmail.com>
2020-02-09 17:41:19 -0800

    add some default pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,34 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        hiit-workout-app
-      </h1>
-      <h2 class="subtitle">
-        HIIT Workout Generator
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            HIIT Generator
+          </h1>
+          <h2 class="subtitle">
+            High intensity interval training workouts a the click of a button.
+          </h2>
+          <p>
+            <nuxt-link class="button is-primary" to="/workout">
+              <strong>START A WORKOUT</strong>
+            </nuxt-link>
+          </p>
+        </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
+
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style lang="scss" scoped>
+.hero {
+  background: rgb(40,49,73);
+  background: linear-gradient(142deg, rgba(40,49,73,1) 0%, rgba(40,49,73,1) 35%, rgba(85,96,124,1) 100%);
 }
 </style>
